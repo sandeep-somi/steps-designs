11fa417765f31c62ecc9fe41e6c1bfa07cd7b739
Sandeep Singh <soami.ssr@gmail.com>
2019-04-14 16:14:35 +0530

    Updates

---
+++ precache-manifest.bbc99e02c7c43be847d2a10c02d65805.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5c8e32fcef1cd58bdcfa",
+    "url": "/steps-designs/static/js/runtime~main.4bc9a351.js"
+  },
+  {
+    "revision": "afb93b3e7bdef891366e",
+    "url": "/steps-designs/static/js/main.2674757e.chunk.js"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/steps-designs/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "afb93b3e7bdef891366e",
+    "url": "/steps-designs/static/css/main.857dad67.chunk.css"
+  },
+  {
+    "revision": "34ce6cc84f4e3d45c621e3d15ed20820",
+    "url": "/steps-designs/index.html"
+  }
+];
